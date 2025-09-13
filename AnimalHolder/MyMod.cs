0020f34143c57872999c94d3ba3d98f333758e00
Jesús Estor <animaleco@users.noreply.github.com>
2025-09-13 19:30:48 +0200

    Minimal working example

--- AnimalHolder/MyMod.cs
+++ AnimalHolder/MyMod.cs
@@ -5,9 +5,34 @@ using BepInEx;
 using UnityEngine.Networking;
 using BepInEx.Configuration;
 using System.IO;
+using GlobalEnums;
 
 [BepInPlugin("com.animaleco.silksong.mymod", "MyMod", "1.0.0")]
 public class MyMod : BaseUnityPlugin
 {
-  
+   private bool isPlayerConfigured = false;
+
+   public void PrintDeath()
+   {
+      Logger.LogInfo("RIP");
+
+   }
+
+   public void Update()
+   {
+      // Check if is playing
+      if (GameManager._instance.GameState == GlobalEnums.GameState.PLAYING)
+      {
+         if (!isPlayerConfigured)
+            ConfigurePlayer();
+      }
+   }
+
+   public void ConfigurePlayer()
+   {
+      Logger.LogInfo("Enter configured player");
+      HeroController.instance.OnDeath += PrintDeath;
+      isPlayerConfigured = true;
+      Logger.LogInfo("Configured player");
+   }
 }
