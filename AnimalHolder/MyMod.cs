f77657da659b278187591cb3c15b3f82ac87c05b
Jesús Estor <animaleco@users.noreply.github.com>
2025-09-13 19:01:11 +0200

    Create project placeholder

---
+++ AnimalHolder/MyMod.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System;
+using UnityEngine;
+using BepInEx;
+using UnityEngine.Networking;
+using BepInEx.Configuration;
+using System.IO;
+
+[BepInPlugin("com.animaleco.silksong.mymod", "MyMod", "1.0.0")]
+public class MyMod : BaseUnityPlugin
+{
+  
+}
